5e5c7e037f48a0a51335c89a1a250d7db38aeeea
Perth Charles <zhongbincharles@gmail.com>
2015-11-23 23:19:46 +0800

    tcp congestion control: part 6 Scalable algorithm

--- linux-3.10/net/ipv4/tcp_scalable.c
+++ linux-3.10/net/ipv4/tcp_scalable.c
@@ -25,12 +25,20 @@ static void tcp_scalable_cong_avoid(struct sock *sk, u32 ack, u32 in_flight)
 	if (tp->snd_cwnd <= tp->snd_ssthresh)
 		tcp_slow_start(tp);
 	else
+        /* scalable算法核心1 */
+        /* 如果cwnd < 50, 则使用Reno一样的策略增长cwnd
+         * 主要是限制cwnd较大时的AI值为一个常数：50
+         * 因此如果这种情况下要恢复到进入快速重传时的cwnd值，比如从reduced增长回original，
+         * 那么需要的恢复时间就是(original - reduced)/50个RTT，而reduced = 7/8 * original
+         * 故恢复时间为original/(50 * 8)个RTT */
 		tcp_cong_avoid_ai(tp, min(tp->snd_cwnd, TCP_SCALABLE_AI_CNT));
 }
 
 static u32 tcp_scalable_ssthresh(struct sock *sk)
 {
 	const struct tcp_sock *tp = tcp_sk(sk);
+    /* scalable算法核心2 */
+    /* 慢启动阈值设置为7/8*cwnd */
 	return max(tp->snd_cwnd - (tp->snd_cwnd>>TCP_SCALABLE_MD_SCALE), 2U);
 }
 
