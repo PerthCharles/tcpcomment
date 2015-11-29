df474a67c017334a1ce89bfa636c8757253efd02
Perth Charles <zhongbincharles@gmail.com>
2015-11-29 22:47:21 +0800

    tcp congestion control: part 8 westwood

--- linux-3.10/net/ipv4/tcp_westwood.c
+++ linux-3.10/net/ipv4/tcp_westwood.c
@@ -29,13 +29,21 @@
 
 /* TCP Westwood structure */
 struct westwood {
+    /* 计算bw_est的一个中间值，用于平滑采样 */
 	u32    bw_ns_est;        /* first bandwidth estimation..not too smoothed 8) */
+    /* 根据经过平滑过一次的bw_ns_est值，再计算最终的bw_est */
 	u32    bw_est;           /* bandwidth estimate */
+    /* 采样周期的起点 */
 	u32    rtt_win_sx;       /* here starts a new evaluation... */
+    /* 在测量时间内确认的字节数 */
 	u32    bk;
+    /* 记录处理完某一个数据包之前的snd_una，用于计算bk */
 	u32    snd_una;          /* used for evaluating the number of acked bytes */
+    /* 慢速路径下，被一个ACK包确认的字节数 */
 	u32    cumul_ack;
+    /* 当前被接收端乱序接收到的字节数 */
 	u32    accounted;
+    /* 每收到一个ACK，获取的一个rtt sample */
 	u32    rtt;
 	u32    rtt_min;          /* minimum observed RTT */
 	u8     first_ack;        /* flag which infers that this is the first ack */
@@ -44,7 +52,7 @@ struct westwood {
 
 
 /* TCP Westwood functions and constants */
-#define TCP_WESTWOOD_RTT_MIN   (HZ/20)	/* 50ms */
+#define TCP_WESTWOOD_RTT_MIN   (HZ/20)	/* 50ms */      /* 带宽至少50ms更新一次 */
 #define TCP_WESTWOOD_INIT_RTT  (20*HZ)	/* maybe too conservative?! */
 
 /*
@@ -58,6 +66,7 @@ struct westwood {
  * way as soon as possible. It will reasonably happen within the first
  * RTT period of the connection lifetime.
  */
+/* westwood初始化函数，在TCP流初始化拥塞控制算法时调用 */
 static void tcp_westwood_init(struct sock *sk)
 {
 	struct westwood *w = inet_csk_ca(sk);
@@ -68,7 +77,7 @@ static void tcp_westwood_init(struct sock *sk)
 	w->accounted = 0;
 	w->cumul_ack = 0;
 	w->reset_rtt_min = 1;
-	w->rtt_min = w->rtt = TCP_WESTWOOD_INIT_RTT;
+	w->rtt_min = w->rtt = TCP_WESTWOOD_INIT_RTT;    /* 在未获得RTT sample前，默认使用20s作为RTT，真是太保守了 */
 	w->rtt_win_sx = tcp_time_stamp;
 	w->snd_una = tcp_sk(sk)->snd_una;
 	w->first_ack = 1;
@@ -78,18 +87,23 @@ static void tcp_westwood_init(struct sock *sk)
  * @westwood_do_filter
  * Low-pass filter. Implemented using constant coefficients.
  */
+/* ret_val = 7/8 * a + 1/8 * b，对采样进行平滑处理的公式
+ * 调用时基本都是返回给a，即ret_val就是a */
 static inline u32 westwood_do_filter(u32 a, u32 b)
 {
 	return ((7 * a) + b) >> 3;
 }
 
+/* 计算带宽估计值，delta表示时间周期长度，w->bk表示这个周期内被确认的字节数 */
 static void westwood_filter(struct westwood *w, u32 delta)
 {
 	/* If the filter is empty fill it with the first sample of bandwidth  */
+    /* 第一次采样时，也就没有平滑的必要的 */
 	if (w->bw_ns_est == 0 && w->bw_est == 0) {
 		w->bw_ns_est = w->bk / delta;
 		w->bw_est = w->bw_ns_est;
 	} else {
+        /* 调用westwood_do_filter()对 "w->bk / delta"计算的带宽值进行平滑处理 */
 		w->bw_ns_est = westwood_do_filter(w->bw_ns_est, w->bk / delta);
 		w->bw_est = westwood_do_filter(w->bw_est, w->bw_ns_est);
 	}
@@ -100,6 +114,8 @@ static void westwood_filter(struct westwood *w, u32 delta)
  * Called after processing group of packets.
  * but all westwood needs is the last sample of srtt.
  */
+/* westwood仅使用单一的rtt sample
+ * 函数功能：获得一个有效的rtt 采样时，更新rtt值 */
 static void tcp_westwood_pkts_acked(struct sock *sk, u32 cnt, s32 rtt)
 {
 	struct westwood *w = inet_csk_ca(sk);
@@ -113,15 +129,22 @@ static void tcp_westwood_pkts_acked(struct sock *sk, u32 cnt, s32 rtt)
  * It updates RTT evaluation window if it is the right moment to do
  * it. If so it calls filter for evaluating bandwidth.
  */
+/* 每经过max(w->rtt, 50ms)长的时间，更新一次带宽估计
+ * 点评：
+ *      1. w->rtt的更新是直接使用的rtt采样，并没有进行过平滑，这点比较不合理
+ *      2. 最小的更新周期为50ms，是否太长了点?
+ *      3. westwood没有对rtt平滑，而是对计算的带宽估计值进行平滑，是否有点本末倒置 */
 static void westwood_update_window(struct sock *sk)
 {
 	struct westwood *w = inet_csk_ca(sk);
+    /* 计算距离采样周期开始时间的间隔 */
 	s32 delta = tcp_time_stamp - w->rtt_win_sx;
 
 	/* Initialize w->snd_una with the first acked sequence number in order
 	 * to fix mismatch between tp->snd_una and w->snd_una for the first
 	 * bandwidth sample
 	 */
+    /* first_ack的作用其实也就是保证刚开始采样时，snd_una的准确性 */
 	if (w->first_ack) {
 		w->snd_una = tcp_sk(sk)->snd_una;
 		w->first_ack = 0;
@@ -137,15 +160,20 @@ static void westwood_update_window(struct sock *sk)
 	 * right_bound = left_bound + WESTWOOD_RTT_MIN
 	 */
 	if (w->rtt && delta > max_t(u32, w->rtt, TCP_WESTWOOD_RTT_MIN)) {
+        /* 如果采样时间已经超过max(w->rtt, 50ms)，则更新带宽估计值 */
 		westwood_filter(w, delta);
 
+        /* 开始下一个采样周期 */
 		w->bk = 0;
 		w->rtt_win_sx = tcp_time_stamp;
 	}
 }
 
+/* 更新rtt_min */
 static inline void update_rtt_min(struct westwood *w)
 {
+    /* 在进入Loss时，会设置重设rtt_min标记。
+     * 内含：进入Loss意味着发生重大的链路问题，有可能路由选路也改变了，因此需要更新rtt_min */
 	if (w->reset_rtt_min) {
 		w->rtt_min = w->rtt;
 		w->reset_rtt_min = 0;
@@ -160,6 +188,7 @@ static inline void update_rtt_min(struct westwood *w)
  * header prediction is successful. In such case in fact update is
  * straight forward and doesn't need any particular care.
  */
+/* 快速路径时，更新带宽估计、bk(被确认的字节数)、rtt_min */
 static inline void westwood_fast_bw(struct sock *sk)
 {
 	const struct tcp_sock *tp = tcp_sk(sk);
@@ -167,6 +196,7 @@ static inline void westwood_fast_bw(struct sock *sk)
 
 	westwood_update_window(sk);
 
+    /* 快速路径，说明处于Open状态，直接使用新的snd_una减去上次的值，就能得到被确认的数据量 */
 	w->bk += tp->snd_una - w->snd_una;
 	w->snd_una = tp->snd_una;
 	update_rtt_min(w);
@@ -177,33 +207,50 @@ static inline void westwood_fast_bw(struct sock *sk)
  * This function evaluates cumul_ack for evaluating bk in case of
  * delayed or partial acks.
  */
+/* 慢速路径时，更新被此ACK确认的字节数 
+ * 感觉这个计算的太不严谨了，挂不得westwood在对重传阶段的带宽估计不准确 */
 static inline u32 westwood_acked_count(struct sock *sk)
 {
 	const struct tcp_sock *tp = tcp_sk(sk);
 	struct westwood *w = inet_csk_ca(sk);
 
+    /* 计算此ACK确认的字节数，第一步计算被按序确认的字节数
+     * 1. delayed ack，或者前面有被丢失的ack时，此值是有可能非零的
+     * 2. 如果是dupack，此时为零 */
 	w->cumul_ack = tp->snd_una - w->snd_una;
 
 	/* If cumul_ack is 0 this is a dupack since it's not moving
 	 * tp->snd_una.
 	 */
 	if (!w->cumul_ack) {
+        /* 记录接收端乱序收到的字节数，如果是一个dupack，则增加一个MSS值 */
 		w->accounted += tp->mss_cache;
-		w->cumul_ack = tp->mss_cache;
+        /* 一个dupack也意味着对端收到了一个数据包，尽管是乱序的
+         * 此时将被该ACK确认的字节数设置为一个mss */
+		w->cumul_ack = tp->mss_cache;       
 	}
 
+    /* 如果cumul_ack大于mss_cache，则意味着不是一个dupack */
 	if (w->cumul_ack > tp->mss_cache) {
 		/* Partial or delayed ack */
+        /* 如果是一个partial ack(累计确认的字节数还没有超过对端乱序接收的字节数)，
+         * 那么更新对端依然处于乱序状态的字节数(accounted - cumul_ack)，
+         * 同时假设：这个partial ack仅确认了一个数据包，其他包都已经被dupack确认过
+         * 点评：westwood一直被诟病在重传阶段计算被确认的字节数不准确，我想这个地方就是一个原因
+         * 一个partial ack并不一定仅确认了一个数据包 */
 		if (w->accounted >= w->cumul_ack) {
 			w->accounted -= w->cumul_ack;
 			w->cumul_ack = tp->mss_cache;
 		} else {
-			w->cumul_ack -= w->accounted;
+        /* 如果是一个delayed ack(对应情况应该是cumul_ack==2mss, accounted=0),
+         * 或者是结束recovery时的那个ack(对应：cumul_ack应该覆盖所有接收端乱序收到的数据)
+         * 点评：cumul_ack超过accounted也不一定意味着结束了recovery啊，所以感觉还是不够准确 */
+			w->cumul_ack -= w->accounted;   /* 减掉已经被dupack所确认的字节数 */
 			w->accounted = 0;
 		}
 	}
 
-	w->snd_una = tp->snd_una;
+	w->snd_una = tp->snd_una;   /* 更新snd_una */
 
 	return w->cumul_ack;
 }
@@ -215,6 +262,8 @@ static inline u32 westwood_acked_count(struct sock *sk)
  * in packets we use mss_cache). Rttmin is guaranteed to be >= 2
  * so avoids ever returning 0.
  */
+/* 根据估计的带宽，和rtt_min来设置拥塞窗口和慢启动阈值
+ * rtt_min相当于是链路没有排队时的时延，bw_est相当于是当前流可以占用的带宽 */
 static u32 tcp_westwood_bw_rttmin(const struct sock *sk)
 {
 	const struct tcp_sock *tp = tcp_sk(sk);
@@ -222,6 +271,7 @@ static u32 tcp_westwood_bw_rttmin(const struct sock *sk)
 	return max_t(u32, (w->bw_est * w->rtt_min) / tp->mss_cache, 2);
 }
 
+/* westwood算法的入口函数 */
 static void tcp_westwood_event(struct sock *sk, enum tcp_ca_event event)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
@@ -229,13 +279,17 @@ static void tcp_westwood_event(struct sock *sk, enum tcp_ca_event event)
 
 	switch (event) {
 	case CA_EVENT_FAST_ACK:
+        /* 处于快速路径，更新带宽估计 */
 		westwood_fast_bw(sk);
 		break;
 
+    /* 在退出快速重传时，根据带宽估计，重新设置cwnd和ssthresh。
+     * 所谓重新设置，就是会覆盖快速重传/快速恢复算法调整的cwnd值 */
 	case CA_EVENT_COMPLETE_CWR:
 		tp->snd_cwnd = tp->snd_ssthresh = tcp_westwood_bw_rttmin(sk);
 		break;
 
+    /* 在经历RTO超时时，cwnd会被设置为1，慢启动阈值会根据带宽估计更新 */
 	case CA_EVENT_LOSS:
 		tp->snd_ssthresh = tcp_westwood_bw_rttmin(sk);
 		/* Update RTT_min when next ack arrives */
@@ -243,6 +297,7 @@ static void tcp_westwood_event(struct sock *sk, enum tcp_ca_event event)
 		break;
 
 	case CA_EVENT_SLOW_ACK:
+        /* 慢速路径时，更新带宽估计和被该ACK确认的字节数 */
 		westwood_update_window(sk);
 		w->bk += westwood_acked_count(sk);
 		update_rtt_min(w);
